526c16476f44e34494a00c3a59fd91bfdd98dda9
Dertzey <45997915+Dertzey@users.noreply.github.com>
2023-08-25 14:54:25 +0200

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-// consumer_key=${consumerKey}&consumer_secret=${consumerSecret}
-const BASEURL = `https://dertzey.no/wp-json/`; 
-const ALLPRODUCTS = `wc/store/products/`
-const SINGLEPRODUCT = "wc/store/products/"
-const consumerKey = 'ck_596d9e80f6238d8b652e40b81eedbc17fbba5ce3'
-const consumerSecret = 'cs_4ea8de88794ac17ee77ca7e67c776c40293f9457'
-
