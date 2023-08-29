7067aa9faba1e91afffb687f8bc2b0238552af2b
Dertzey <djeketefra10@hotmail.com>
2023-08-29 19:23:40 +0200

    changes

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 const BASEURL = `https://dertzey.no/wp-json/`; 
 const ALLPRODUCTS = `wc/store/products/`
 const SINGLEPRODUCT = "wc/store/products/"
-const consumerKey = 'ck_596d9e80f6238d8b652e40b81eedbc17fbba5ce3'
-const consumerSecret = 'cs_4ea8de88794ac17ee77ca7e67c776c40293f9457'
+const consumerKey = 'ck_90a4a4b7c1268b8df64774766171e699232a2b9c'
+const consumerSecret = 'cs_e997b9616770f3018c7f9be6a3a52328dce35e7c'
 
