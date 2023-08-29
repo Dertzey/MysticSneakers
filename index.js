c752b6b773fbcf0a150107b6a8691391b7afd325
Dertzey <djeketefra10@hotmail.com>
2023-08-29 19:52:11 +0200

    added headless cms and updated code

--- index.js
+++
@@ -1,7 +0,0 @@
-// consumer_key=${consumerKey}&consumer_secret=${consumerSecret}
-const BASEURL = `https://dertzey.no/wp-json/`; 
-const ALLPRODUCTS = `wc/store/products/`
-const SINGLEPRODUCT = "wc/store/products/"
-const consumerKey = 'ck_90a4a4b7c1268b8df64774766171e699232a2b9c'
-const consumerSecret = 'cs_e997b9616770f3018c7f9be6a3a52328dce35e7c'
-
---
+++ js/shop.js
@@ -0,0 +1,19 @@
+fetch(BASEURL + ALLPRODUCTS)
+  .then(response => response.json())
+  .then(data => {
+    const SNEAKERCONTAINER = document.querySelector(".featured-prods");
+
+    data.forEach(data => {
+      SNEAKERCONTAINER.innerHTML += `
+          <a href="product-page.html?id=${data.id}">
+            <div class="fepro">
+              <img src="${data.images[0].src}" alt="${data.images[0].alt}">
+              <h3>${data.name}</h3>
+              <p>${data.description}</p>
+              <p class="price">${data.price_html}</p>
+            </div>
+          </a>`;
+    });
+  })
+  .catch(error => { console.log('An error occurred:', error); });
+
