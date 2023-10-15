7ee359a4d99aa2f31aa8a19820ff0351d9e568b3
Dertzey <djeketefra10@hotmail.com>
2023-10-15 13:28:19 +0200

    Minor changes

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
 const BASEURL = 'https://cors.noroff.dev/https://dertzey.no/wp-json/';
 const ALLPRODUCTS = 'wc/v3/products'
-const consumerKey = 'consumer_key=ck_90a4a4b7c1268b8df64774766171e699232a2b9c&'
-const consumerSecret = 'consumer_secret=cs_e997b9616770f3018c7f9be6a3a52328dce35e7c'
+const consumerKey = 'consumer_key=ck_604a086e415a68b4d4bb7a9965ee7f29fa7bca9a'
+const consumerSecret = 'consumer_secret=cs_d11cfb00c4544376249f0ca789a31fea6189ca95'
