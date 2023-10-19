c90af0c16047a11f4264162fcd6a74c7ddf64828
Dertzey <djeketefra10@hotmail.com>
2023-10-19 22:02:44 +0200

    Change in code

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
 const BASEURL = 'https://cors.noroff.dev/https://dertzey.no/wp-json/';
 const ALLPRODUCTS = 'wc/v3/products'
-const consumerKey = 'consumer_key=ck_3fa5db4b61bf4176859c8b9466c40ce24df0ff83';
-const consumerSecret = 'cs_953243e55fa6f6c83ea387cb23add51dd07bd9d2';
+const consumerKey = 'consumer_key=ck_25b078f4a8e1d2f83bc9f5c24b2d2718f5e05f20&';
+const consumerSecret = 'consumer_secret=cs_25271461c2e3eb451d77951e16988bc22c626108';
--- js/shop.js
+++ js/shop.js
@@ -1,19 +1,20 @@
-fetch(`${BASEURL}${ALLPRODUCTS}?consumer_key=${consumerKey}&consumer_secret=${consumerSecret}`)
+fetch(`${BASEURL}${ALLPRODUCTS}?consumer_key=ck_25b078f4a8e1d2f83bc9f5c24b2d2718f5e05f20&consumer_secret=cs_25271461c2e3eb451d77951e16988bc22c626108`)
   .then(response => response.json())
   .then(data => {
     const SNEAKERCONTAINER = document.querySelector(".featured-prods");
 
-    data.forEach(data => {
+    data.forEach(product => {
       SNEAKERCONTAINER.innerHTML += `
-          <a href="product-page.html?id=${data.id}">
+          <a href="product-page.html?id=${product.id}">
             <div class="fepro">
-              <img src="${data.images[0].src}" alt="${data.images[0].alt}">
-              <h3>${data.name}</h3>
-              <p>${data.description}</p>
-              <p class="price">${data.price_html}</p>
+              <img src="${product.images[0].src}" alt="${product.images[0].alt}">
+              <h3>${product.name}</h3>
+              <p>${product.description}</p>
+              <p class="price">${product.price_html}</p>
             </div>
           </a>`;
     });
   })
   .catch(error => { console.log('An error occurred:', error); });
+
 
